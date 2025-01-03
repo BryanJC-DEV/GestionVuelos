32d7471d316326524e19f746f937e39dbb36cffc
BryanJC <106637053+BryanJC-DEV@users.noreply.github.com>
2025-01-02 22:09:59 -0600

    Add files via upload

---
+++ GestionVuelos_Servidor/src/gestionvuelos_servidor/GestionVuelos_Servidor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestionvuelos_servidor;
+
+/**
+ *
+ * @author bryan
+ */
+public class GestionVuelos_Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n=2;
+    }
+    
+}
